2b3b0abbe3d39acd55851165a0be0813147719e6
justusjb <jujo03h@icloud.com>
2024-04-06 22:43:58 +0200

    Added placeholder for new code

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import requests
 from typing import List
 import os
 
+
 app = FastAPI()
 
 app.add_middleware(
@@ -44,7 +45,7 @@ async def get_file(repo_url: str, file_path: str, branch: str = "main"):
 
     print("Response status code: ", response.status_code)
     print("Response content: ", response.content)
-    
+
     if response.status_code == 200:
         # Return the file content
         # You might want to return the content type as well depending on the file
@@ -85,4 +86,54 @@ async def get_directory_contents(repo_url: str, dir_path: str):
         return directory_contents
     else:
         raise HTTPException(status_code=response.status_code, detail="Failed to fetch directory contents")
+
+
+
+@app.get("/get-new-code/")
+async def get_new_code(old_code: str):
+
+    long_code = """
+    import streamlit as st
+    import pandas as pd
+    import numpy as np
+    
+    st.title('Uber pickups in NYC')
+    
+    DATE_COLUMN = 'date/time'
+    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+                
+                @st.cache
+                def load_data(nrows):
+                    data = pd.read_csv(DATA_URL, nrows=nrows)
+                    lowercase = lambda x: str(x).lower()
+                    data.rename(lowercase, axis='columns', inplace=True)
+                    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+                    return data
+                    
+                    data_load_state = st.text('Loading data...')
+                    data = load_data(10000)
+                    data_load_state.text("Done! (using st.cache)")
+                
+                if st.checkbox('Show raw data'):
+                    st.subheader('Raw data')
+                    st.write(data)
+                    
+                    st.subheader('Number of pickups by hour')
+                    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+                    st.bar_chart(hist_values)
+                    
+                    # Some number in the range 0-23
+                    hour_to_filter = st.slider('hour', 0, 23, 17)
+                    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+                    
+                    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+                    st.map(filtered_data)
+                    
+        """
+
+    print(long_code)
+
+
+
 
