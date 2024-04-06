90342b94e01ad6bd2e2e00151505a101ff02053f
IceboxDev <mantas_kandratavicius@yahoo.com>
2024-04-07 01:55:19 +0200

    Expand tokenizer to support multiple languages

--- backend/prompt_constants.py
+++ backend/prompt_constants.py
@@ -1,16 +1,18 @@
 import typing
 
 PROMPT_EN: typing.Final[str] = (
-    "Starting from now, you're a computer science expert. Given the following "
-    "code, rewrite it so it has identical functionality in the "
-    "language that is being asked for. Your response should be code and code "
-    "only. Here's the code from github repository that you need to rewrite:"
-    "\n\n"
+    "The GPT should focus solely on translating provided code snippets into "
+    "Python, ensuring the translated Python code maintains identical "
+    "functionality to the original code. The GPT should not omit any part of "
+    "the code, ensuring a complete and functional Python translation. The "
+    "responses should exclusively contain Python code, with no additional "
+    "explanations or comments, unless they are essential for understanding or "
+    "running the Python code. The GPT should avoid engaging in discussions or "
+    "providing insights outside the scope of code translation.\n\n"
 )
 
 PROMPT_TEMPLATE_EN: typing.Final[str] = (
-    PROMPT_EN
-    + """
+    PROMPT_EN + """
 {context}
 -----
 Request: {question}
--- tokenizer/data.py
+++ tokenizer/data.py
@@ -1,12 +1,66 @@
-from langchain_community.document_loaders.parsers import LanguageParser
+from langchain_community.document_loaders.parsers.language.javascript import JavaScriptSegmenter
+from langchain_community.document_loaders.parsers.language.typescript import TypeScriptSegmenter
+from langchain_community.document_loaders.parsers.language.csharp import CSharpSegmenter
+from langchain_community.document_loaders.parsers.language.kotlin import KotlinSegmenter
+from langchain_community.document_loaders.parsers.language.python import PythonSegmenter
+from langchain_community.document_loaders.parsers.language.cobol import CobolSegmenter
+from langchain_community.document_loaders.parsers.language.scala import ScalaSegmenter
+from langchain_community.document_loaders.parsers.language.java import JavaSegmenter
+from langchain_community.document_loaders.parsers.language.perl import PerlSegmenter
+from langchain_community.document_loaders.parsers.language.ruby import RubySegmenter
+from langchain_community.document_loaders.parsers.language.rust import RustSegmenter
+from langchain_community.document_loaders.parsers.language.cpp import CPPSegmenter
+from langchain_community.document_loaders.parsers.language.lua import LuaSegmenter
+from langchain_community.document_loaders.parsers.language.go import GoSegmenter
+from langchain_community.document_loaders.parsers.language.c import CSegmenter
 from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.blob_loaders import Blob
 from langchain_core.documents.base import Document
 from langchain_text_splitters import Language
+
+from langchain_community.document_loaders.parsers import LanguageParser
 
 import pickle
 import typing
 import tqdm
 import os
+
+
+LANGUAGE_EXTENSIONS: typing.Dict[str, str] = {
+    "py": "python",
+    "js": "js",
+    "cobol": "cobol",
+    "c": "c",
+    "cpp": "cpp",
+    "cs": "csharp",
+    "rb": "ruby",
+    "scala": "scala",
+    "rs": "rust",
+    "go": "go",
+    "kt": "kotlin",
+    "lua": "lua",
+    "pl": "perl",
+    "ts": "ts",
+    "java": "java",
+}
+
+LANGUAGE_SEGMENTERS: typing.Dict[str, typing.Any] = {
+    "python": PythonSegmenter,
+    "js": JavaScriptSegmenter,
+    "cobol": CobolSegmenter,
+    "c": CSegmenter,
+    "cpp": CPPSegmenter,
+    "csharp": CSharpSegmenter,
+    "ruby": RubySegmenter,
+    "rust": RustSegmenter,
+    "scala": ScalaSegmenter,
+    "go": GoSegmenter,
+    "kotlin": KotlinSegmenter,
+    "lua": LuaSegmenter,
+    "perl": PerlSegmenter,
+    "ts": TypeScriptSegmenter,
+    "java": JavaSegmenter,
+}
 
 
 class TqdmFileWrapper:
@@ -27,6 +81,67 @@ class TqdmFileWrapper:
     # Add any other methods from the file object you might need
     def close(self):
         self._file.close()
+
+
+class CustomParser(LanguageParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def lazy_parse(self, blob: Blob) -> typing.Iterator[Document]:
+        code = blob.as_string()
+
+        language = self.language or (
+            LANGUAGE_EXTENSIONS.get(blob.source.rsplit(".", 1)[-1])
+            if isinstance(blob.source, str)
+            else None
+        )
+
+        if language is None:
+            yield Document(
+                page_content=code,
+                metadata={
+                    "source": blob.source,
+                },
+            )
+            return
+
+        if self.parser_threshold >= len(code.splitlines()):
+            yield Document(
+                page_content=code,
+                metadata={
+                    "source": blob.source,
+                    "language": language,
+                },
+            )
+            return
+
+        self.Segmenter = LANGUAGE_SEGMENTERS[language]
+        segmenter = self.Segmenter(blob.as_string())
+        if not segmenter.is_valid():
+            yield Document(
+                page_content=code,
+                metadata={
+                    "source": blob.source,
+                },
+            )
+            return
+
+        for functions_classes in segmenter.extract_functions_classes():
+            yield Document(
+                page_content=functions_classes,
+                metadata={
+                    "source": blob.source,
+                    "content_type": "functions_classes",
+                    "language": language,
+                },
+            )
+        yield Document(
+            page_content=code,
+            metadata={
+                "source": blob.source,
+                "language": language,
+            },
+        )
 
 
 class Data:
@@ -62,11 +177,10 @@ class Data:
             glob="*",
             # exclude=Data.BINARY_EXCLUDES,
             suffixes=[".cbl"],
-            parser=LanguageParser(language=Language.COBOL),
+            parser=CustomParser(language=Language.COBOL),
         )
 
         self.data = loader.load()
-        print(len(self.data))
 
     def load_from_pickle(self, file_path=PICKLE_PATH):
         file_size = os.path.getsize(file_path)
@@ -78,7 +192,6 @@ class Data:
             self.data = pickle.load(wrapped_file)
 
     def export_data(self):
-        # Create the data directory if it doesn't exist
         os.makedirs("data", exist_ok=True)
 
         with open(Data.PICKLE_PATH, "wb") as f:
