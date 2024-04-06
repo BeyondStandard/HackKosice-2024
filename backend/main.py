a84558c4b41e8931057ca2348b3bb9274908d239
justusjb <jujo03h@icloud.com>
2024-04-06 20:28:18 +0200

    Added authorization to GitHub API calls to prevent rate limiting

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 from typing import List
+import os
 
 app = FastAPI()
 
@@ -13,6 +14,13 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+
+def get_github_token_header():
+    github_token = os.getenv('GITHUB_TOKEN')
+    headers = {
+        "Authorization": f"Bearer {github_token}",
+    }
+    return headers
 
 @app.get("/")
 async def root():
@@ -32,7 +40,7 @@ async def get_file(repo_url: str, file_path: str, branch: str = "main"):
     raw_url = f"https://raw.githubusercontent.com/{repo_url}/{branch}/{file_path}"
 
     # Fetch the file content
-    response = requests.get(raw_url)
+    response = requests.get(raw_url, headers=get_github_token_header())
     if response.status_code == 200:
         # Return the file content
         # You might want to return the content type as well depending on the file
@@ -47,7 +55,7 @@ async def get_repo_structure(repo_url: str):
     # Adjust based on the structure of 'repo_url' you expect
     api_url = f"https://api.github.com/repos/{repo_url}/contents/"
 
-    response = requests.get(api_url)
+    response = requests.get(api_url, headers=get_github_token_header())
     print("Response status code: ", response.status_code)
     print("Response content: ", response.content)
     if response.status_code == 200:
@@ -64,7 +72,7 @@ async def get_directory_contents(repo_url: str, dir_path: str):
     # Construct the GitHub API URL for getting the contents of the directory
     api_url = f"https://api.github.com/repos/{repo_url}/contents/{dir_path}"
 
-    response = requests.get(api_url)
+    response = requests.get(api_url, headers=get_github_token_header())
     print("Response status code: ", response.status_code)
     print("Response content: ", response.content)
     if response.status_code == 200:
