a9974fef90c597ca6cdaf5aaedd53ead08759efc
INNO\bmyrinets <bohdan.myrinets@innovatrics.com>
2024-04-07 09:29:38 +0200

    ffdig

--- server.js
+++ server.js
@@ -13,46 +13,52 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/getAccessToken", async function (req, res) {
-  const params =
-    "?client_id=" +
-    CLIENT_ID +
-    "&client_secret=" +
-    CLIENT_SECRET +
-    "&code=" +
-    req.query.code;
+app.get(
+  "https://televateyourcodewith.us:5000/getAccessToken",
+  async function (req, res) {
+    const params =
+      "?client_id=" +
+      CLIENT_ID +
+      "&client_secret=" +
+      CLIENT_SECRET +
+      "&code=" +
+      req.query.code;
 
-  await fetch("https://github.com/login/oauth/access_token" + params, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      return response.json();
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
     })
-    .then((data) => {
-      console.log(data);
-      res.json(data);
-    });
-});
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        res.json(data);
+      });
+  }
+);
 
-app.get("/getUserData", async function (req, res) {
-  req.get("Authorization"); //bearer access token
-  await fetch("https://api.github.com/user", {
-    method: "GET",
-    headers: {
-      Authorization: req.get("Authorization"),
-    },
-  })
-    .then((response) => {
-      return response.json();
+app.get(
+  "https://televateyourcodewith.us:5000/getUserData",
+  async function (req, res) {
+    req.get("Authorization"); //bearer access token
+    await fetch("https://api.github.com/user", {
+      method: "GET",
+      headers: {
+        Authorization: req.get("Authorization"),
+      },
     })
-    .then((data) => {
-      console.log(data);
-      res.json(data);
-    });
-});
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        res.json(data);
+      });
+  }
+);
 
 app.listen(4000, function () {
   console.log("CORS server running on port 4000");
